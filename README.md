5a402ad4a6bd65c1b0e3a99118601f6683a990bb
Rokas Aniss <rokas.anisas@gmail.com>
2020-07-18 17:48:03 +0300

    prettier + lintfix

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## React Template by [@RokasAniss](https://github.com/RokasAniss)
 
 ### Structure `(src/)`
+
 - `src/components` UI components only, no direct connection to store or api calls in here
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,19 @@
-import * as path from "path";
-import * as webpack from "webpack";
+import * as path from 'path';
+import * as webpack from 'webpack';
 
 const config: webpack.Configuration = {
-  mode: "production",
-  entry: "./src/index.ts",
+  mode: 'production',
+  entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "foo.bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: [/node_modules/],
-        use: ["ts-loader"],
+        use: ['ts-loader'],
       },
     ],
   },
