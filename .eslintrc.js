42d2b630bb64999b21f4343494c31e717ab2abce
Rokas Aniss <rokas.anisas@gmail.com>
2020-07-18 20:38:27 +0300

    eslint xtend

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
   rules: {
     strict: 0,
     semi: 'warn',
