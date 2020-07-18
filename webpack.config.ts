bc8de129b3a27742d7f138f877a4a7800702283a
Rokas Aniss <rokas.anisas@gmail.com>
2020-07-18 17:44:37 +0300

    build ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,10 +3,19 @@ import * as webpack from "webpack";
 
 const config: webpack.Configuration = {
   mode: "production",
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "foo.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: [/node_modules/],
+        use: ["ts-loader"],
+      },
+    ],
   },
 };
 
