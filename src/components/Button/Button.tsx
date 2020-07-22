aa5b9180983e73d55497743eacba7ef1035c719b
Rokas <rokas.anisas@gmail.com>
2020-07-22 13:41:18 +0300

    [ RT-3 ] button adjustments

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,11 @@ import React, { FunctionComponent } from 'react';
 
 import './Button.scss';
 
-const Button: FunctionComponent<ButtonProps> = ({ title }: ButtonProps) => {
+const Button: FunctionComponent<ButtonProps> = ({ title, disabled }: ButtonProps) => {
   const className = "button";
 
   return (
-    <button className={className}>
+    <button className={className} disabled={disabled}>
       <span className={`${className}__title`}>{title}</span>
     </button>
   );
@@ -14,6 +14,7 @@ const Button: FunctionComponent<ButtonProps> = ({ title }: ButtonProps) => {
 
 interface ButtonProps {
   title?: string;
+  disabled?: boolean;
 }
 
 export default Button;
