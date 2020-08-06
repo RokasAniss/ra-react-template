8e2ed3d55669361cca84d61873d16fd90b5dc74a
Rokas <rokas.anisas@gmail.com>
2020-08-06 23:08:10 +0300

    redux init

---
+++ src/store/initialState.ts
@@ -0,0 +1,7 @@
+import { ApplicationState } from './types/applicationState';
+
+const initialState: ApplicationState = {
+  title: 'Logo',
+};
+
+export default initialState;
---
+++ src/store/types/actionTypes.ts
@@ -0,0 +1,11 @@
+export interface SetTitle {
+  type: ActionTypes.setTitle;
+  payload: string;
+}
+
+export enum ActionTypes {
+  setTitle = 'SetTitle',
+}
+
+export type Actions =
+  | SetTitle;
