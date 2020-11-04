c1556bbfc926f5f60a4f55f863c89a7baa12e881
Rokas <rokas.anisas@gmail.com>
2020-11-04 23:16:35 +0200

    [ RT-11 ] Routes adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import MainLayout from '@/layouts/MainLayout';
 
@@ -7,11 +7,15 @@ const App: FunctionComponent = () => {
   return (
     <Router>
       <MainLayout>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </div>
         <Switch>
-          <Route path="/">
+          <Route path="/" exact>
             <span>Home</span>
           </Route>
-          <Route path="/about">
+          <Route path="/about" exact>
             <span>About</span>
           </Route>
         </Switch>
