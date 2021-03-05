b86adbac32b3a86d5e37f93d6b83aeba580a9a49
Rokas Anisas <rokas.anisas@gmail.com>
2021-03-05 20:01:27 +0200

    Update actions.ts

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,6 +1,6 @@
 import store from './store';
 import { ActionTypes } from './types/actionTypes';
 
-export const SetChapter = (payload: string): void => {
+export const SetTitle = (payload: string): void => {
   store.dispatch({ type: ActionTypes.setTitle, payload: payload });
 };
