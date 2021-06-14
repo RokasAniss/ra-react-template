13b640d116755ffca3f46cddf3bcd8553f972022
Rokas Anisas <rokas.anisas@gmail.com>
2021-06-14 22:36:11 +0300

    update store module

--- src/store/modules/title/title.actions.ts
+++ src/store/modules/title/title.actions.ts
@@ -1,11 +1,6 @@
-import { store } from '@/store';
-
 import { TitleActionTypes, SetName } from './title.actionTypes';
 
-export const setName = (payload: SetName['payload']): void => {
-  const data: SetName = {
-    type: TitleActionTypes.setName,
-    payload: payload,
-  };
-  store.dispatch(data);
-};
+export const setName = (payload: SetName['payload']): SetName => ({
+  type: TitleActionTypes.setName,
+  payload: payload,
+});
