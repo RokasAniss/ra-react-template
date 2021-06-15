10da8f1d4ec13396e2f3f3a83d23565e17fb974d
Rokas Anisas <rokas.anisas@gmail.com>
2021-06-15 20:39:58 +0300

    use css modules

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,16 +1,18 @@
 import React, { FC } from 'react';
+import classNames from 'classnames/bind';
 
 import raLogo from '@/assets/ra-logo.svg';
 
-import './Logo.scss';
+import styles from './Logo.module.scss';
+const cx = classNames.bind(styles);
 
 const Logo: FC = () => {
   const className = 'logo';
 
   return (
-    <div className={className}>
-      <img src={raLogo} className={`${className}__icon`} alt="logo" />
-      <span className={`${className}__title`}>ra react template</span>
+    <div className={cx(className)}>
+      <img src={raLogo} className={cx(`${className}__icon`)} alt="logo" />
+      <span className={cx(`${className}__title`)}>ra react template</span>
     </div>
   );
 };
