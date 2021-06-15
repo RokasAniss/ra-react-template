42cf802be437eb9d4808fce5a1bbbe9622e44fef
Rokas Anisas <rokas.anisas@gmail.com>
2021-06-15 20:36:54 +0300

    use css modules

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,19 +1,21 @@
 import React, { FC, ReactNode } from 'react';
+import classNames from 'classnames/bind';
 
 import Logo from '@/components/Logo';
 
-import './MainLayout.scss';
+import styles from './MainLayout.module.scss';
+const cx = classNames.bind(styles);
 
 const MainLayout: FC<MainLayoutProps> = ({ children }: MainLayoutProps) => {
   const className = 'main-layout';
 
   return (
-    <div className={className}>
-      <div className={`${className}__container`}>
-        <div className={`${className}__logo`}>
+    <div className={cx(className)}>
+      <div className={cx(`${className}__container`)}>
+        <div className={cx(`${className}__logo`)}>
           <Logo />
         </div>
-        <div className={`${className}__body`}>{children}</div>
+        <div className={cx(`${className}__body`)}>{children}</div>
       </div>
     </div>
   );
